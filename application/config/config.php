87b42de310c40eefff4ef7dfee7415b81c037a1c
tomual <tomual@users.noreply.github.com>
2017-10-03 18:17:10 -0500

    Use database session driver

--- application/config/config.php
+++ application/config/config.php
@@ -367,10 +367,10 @@ $config['encryption_key'] = 'xUwz5Ug9t7z3futgPuI43vC86WHvFS77';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_expiration'] = 0;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
--- sql/database.sql
+++ sql/database.sql
@@ -119,3 +119,13 @@ CREATE TABLE IF NOT EXISTS `versions` (
   `created` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`vid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8;
+
+-- Dumping structure for table tintin.ci_sessions
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `id` varchar(128) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
+  `data` blob NOT NULL,
+  PRIMARY KEY (`id`,`ip_address`),
+  KEY `ci_sessions_timestamp` (`timestamp`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
